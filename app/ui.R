86695c273b533969d0e2f9618e8f03982c24e071
EricKrg <eric.krueger@uni-jena.de>
2018-04-06 08:46:32 +0100

    seperated ui and server

---
+++ app/ui.R
@@ -0,0 +1,173 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+require(rgbif)
+require(tidyverse)
+require(sf)
+require(sp)
+require(rgeos)
+require(data.table)
+require(leaflet)
+require(leaflet.extras)
+require(shiny)
+require(shinydashboard)
+require(plotly)
+require(stplanr)
+require(weatherr)
+require(shinyWidgets)
+require(mapview)
+
+
+# constant like vars.
+apikey <- "AIzaSyAB6DJYmiY-82HLSgo0CLCDeZ9h2p6l9xY"
+cycle_api <- "8e9f2ec7f09a1ff4"
+graph_hopper <- "170ca04c-aef5-4efc-9691-ec8325d934ef"
+help <- TRUE
+#style funs
+progressBar2 <- function (id, value, total = NULL, display_pct = FALSE, size = NULL,
+                          status = NULL, striped = FALSE, title = NULL)
+{
+  if (is.null(total)) {
+    percent <- value
+  }
+  else {
+    percent <- value
+  }
+  if (!is.null(title) | !is.null(total)) {
+    title <- htmltools::tags$span(class = "progress-text",
+                                  title, htmltools::HTML("&nbsp;"))
+
+  }
+  if (is.null(total)) {
+    total <- htmltools::tags$span(class = "progress-number",
+                                  htmltools::tags$b(value, id = paste0(id, "-value")))
+
+  }
+  tagPB <- htmltools::tags$div(class = "progress-group", title,
+                               total, htmltools::tags$div(class = if (!is.null(size))
+                                 paste("progress", size)
+                                 else "progress", htmltools::tags$div(id = id, style = if (percent >
+                                                                                           0)
+                                   paste0("width:",percent, "%;"), style = if (display_pct)
+                                     "min-width: 2em;", class = "progress-bar", class = if (!is.null(status))
+                                       paste0("progress-bar-", status), class = if (striped)
+                                         "progress-bar-striped", role = "progressbar", if (display_pct)
+                                           paste0(percent, "%"))))
+
+}
+
+
+# Define UI for application that draws a histogram
+
+dashboardPage(dashboardHeader(disable = T),
+                dashboardSidebar(disable = T),
+  dashboardBody(
+  fluidRow(
+    box(title ="hikeR",solidHeader = T,background = "black",width = 12)),
+  fluidRow(
+    column(width = 12,
+           box(width = NULL,solidHeader = TRUE,
+               searchInput(width = NULL,"search","Search Region", placeholder = "City or Region",
+                           value = "Jena" ,
+                           btnSearch = icon("search"),
+                           btnReset = icon("remove")),
+               materialSwitch(inputId = "detail", label = "Jump to street level", status = "danger"),
+               fluidRow(
+                 column(width = 4,
+                        valueBoxOutput("weather",width = NULL)
+                 ),
+                 column(width = 4,
+                        valueBoxOutput("percip", width = NULL)
+                 ),
+                 column(width = 4,
+                        valueBoxOutput("temp", width = NULL)
+                 )
+               ),
+               column(width = 7,
+                      leafletOutput("leafmap",height = 700)),
+               column(width = 5,
+                      fluidRow(
+                        tabBox(width = NULL,
+                               tabPanel(icon = icon("tasks"),title = "Trip data",
+                                        tableOutput("height"),
+                                        progressBar2(title = "performance Kilometer (km)" ,id = "pKm",value = 0, status = "danger",
+                                                     display_pct = TRUE),
+                                        progressBar2(title = "horizontal distance (km)", id = "flat",value = 0, status = "info",
+                                                     display_pct = TRUE),
+                                        progressBar2(title = "vertical up (m)" ,id = "up",value = 0, status = "info",
+                                                     display_pct = TRUE),
+                                        progressBar2(title = "vertical down (m)", id = "down",value = 0, status = "info",
+                                                     display_pct = TRUE)
+                               ),
+                               tabPanel(icon = icon("caret-up"),"Airline",plotlyOutput("plot",width = NULL)),
+                               tabPanel(icon = icon("caret-up"),"Route", plotlyOutput("plot_route")),
+                               tabPanel(title = "Trip coords.",icon = icon("map-marker"),
+                                        tableOutput("data"))
+                        )
+                      ),
+                      fluidRow(
+                        column(width = 12,
+                               box(solidHeader = T,background = "black", width = NULL,title = "Routing",
+                                   materialSwitch(inputId = "string_route", label = "Route by adress", status = "danger"),
+                                   conditionalPanel(condition = "input.string_route",
+                                                    materialSwitch(inputId = "more", label = "More then two Adresses", status = "danger"),
+                                                    conditionalPanel(condition = "input.more",
+                                                                     sliderInput(inputId = "waypoints",label = "Nr. of Waypoints",min = 1,max = 15,value = 1)),
+                                                    textInput(inputId = "from",label = "from A",placeholder = "Adress"),
+                                                    textInput(inputId = "to",label = "to B",placeholder = "Adress"),
+                                                    conditionalPanel(condition = "input.more",
+                                                                     uiOutput("waypoints_panel")
+                                                    )),
+                                   conditionalPanel(condition ="input.leafmap_draw_new_feature || input.string_route",
+                                                    radioGroupButtons(inputId = "route_opt",
+                                                                      label = "Routing providers",
+                                                                      choices = c("cycle","GHopper","OSM"),
+                                                                      checkIcon = list(yes = tags$i(class = "fa fa-check-square",
+                                                                                                    style = "color: steelblue"),
+                                                                                       no = tags$i(class = "fa fa-square-o",
+                                                                                                   style = "color: steelblue"))),
+                                                    conditionalPanel(condition = 'input.route_opt == "cycle"',
+                                                                     radioGroupButtons(inputId = "plan",
+                                                                                       label = "Routing style",
+                                                                                       choices = c("fastest","balanced","quietest"),
+                                                                                       status = "danger")),
+                                                    conditionalPanel( condition = 'input.route_opt == "GHopper"',
+                                                                      radioGroupButtons(inputId = "graph",
+                                                                                        label = "Routing style",
+                                                                                        choices = c("foot","hike","bike", "mtb","racingbike"),
+                                                                                        status = "danger")),
+                                                    actionButton("routing","Route"))
+                               ))),
+                      fluidRow(
+                        box(width = 12,background = "black",title = "Traveltime", solidHeader = T,
+                            column(width = 5,
+                                   knobInput("pace", label = "Speed in km/h: ",
+                                             value = 5,
+                                             thickness = 0.3,
+                                             cursor = TRUE,
+                                             width = "70%",
+                                             height = "70%",
+                                             min = 2,max = 50,
+                                             fgColor = "red" ,bgColor = "white"
+                                   )),
+                            column(width = 5,
+                                   tableOutput("traveltime")))
+                      )
+               )
+           )
+    )
+  )
+)
+)
+
+# ui <- dashboardPage(
+#   dashboardHeader(disable = T),
+#   dashboardSidebar(disable = T),
+#   body
+# )
