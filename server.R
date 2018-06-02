a665b23b2c2f1006beb02804f826293f923bf3b0
EricKrg <eric.krueger@uni-jena.de>
2018-06-02 10:48:29 +0200

    added ors routing and reachability function

--- server.R
+++ server.R
@@ -5,8 +5,9 @@ apikey <- Sys.getenv("apikey")
 cycle_api <- Sys.getenv("cycle_api")
 graph_hopper <- Sys.getenv("graph_hopper")
 openweathermap <- Sys.getenv("openweathermap")
-
+ors_api_key(Sys.getenv("ors"))
 help <- TRUE
+
 
 # Define server logic
 
@@ -25,11 +26,13 @@ server <- function(input, output, session) {
   search2 <- reactiveValues(df = NULL)
   goweather <- reactiveValues(df = NULL) # update weather panels if trip is routed
   goUpdate <- reactiveValues(df = NULL)
+  reach <- reactiveValues(df = NULL)
+  reach_tf <- reactiveValues(df = NULL)
   # intial values
   goweather$df <- FALSE
   routed$df <- FALSE
   goUpdate$df <- FALSE
-
+  reach_tf$df <- FALSE
   #ui value dynamic panels for all waypoints-------------
   output$waypoints_panel <- renderUI({
     out <- list()
@@ -53,14 +56,17 @@ server <- function(input, output, session) {
     }
   })  #render the ui dynamically#render the ui dynamically
 
-  #functions -----------
+
+  #functions -------------------------------------------------------------------
+  # turn into package
   create_lines <- function(data){  # line string from multiple input points
     if(!is.null(data)){
-      print(data)
-      lines <- st_sfc(st_linestring(as.matrix(data)))#,crs = 4326)
+      #print(data)
+      lines <- sf::st_sfc(st_linestring(as.matrix(data)))#,crs = 4326)
       return(lines)
     }
   }
+  ##----------------------------------------------------------------------------
   distance <- function(point_a, point_b, unit){
     lat = (point_a[,2] + point_b[,2]) / 2 * 0.01745
     dx = 111.3 * cos(lat) * (point_a[,1] - point_b[,1])
@@ -73,46 +79,35 @@ server <- function(input, output, session) {
       return(distance)
     }
   }
+  ##----------------------------------------------------------------------------
   spatial <- function(data){
     withProgress(message = 'fetching elevation', value = 0.1, {
-      if(class(data)[1] != "SpatialLinesDataFrame"){ #filter results form routing
-        print("normal lines")
-        lines <- create_lines(data)
-      } else {
+      #print(class(data))
+      if(class(data)[1] == "sf"){ #filter results form routing, if it is routed it is allready a line
         lines <- st_as_sf(data)
         st_crs(lines) <- NA
-
+      } else {
+        lines <- create_lines(data)
       }
       incProgress(0.1)
-      print("sampling")
+      #print("sampling")
       trip_length <- SpatialLinesLengths(as(lines,"Spatial"))
-      print(trip_length)
-      lod <- trip_length/100
-      #print(lod) #lvl of detail -> much quicker
+      lod <- trip_length/100 # lvl of detail for longer trips
       if(trip_length < 1) {
         numOfPoints <- 50
       } else {
         numOfPoints  <-  as.numeric(trip_length/lod)
       }
-      print("sf samples")
-      a <- Sys.time()
-      points <- st_line_sample(lines, numOfPoints, type = "regular")
+      points <- st_line_sample(lines, numOfPoints, type = "regular") #sample points on the line
       points <- st_sf(geometry = st_cast(st_sfc(points),to = "POINT"),crs = 4326)
-      print(a- Sys.time())
-      print("samples done")
       #
       incProgress(0.3)
       #
-      a <- Sys.time()
-      start <- st_sf(geometry = st_sfc(st_point(st_coordinates(lines)[1,1:2])),crs = 4326)
-      points <- rbind(start, points)
-      print(a- Sys.time())
-      print("elevation")
-      a <- Sys.time()
 
+      start <- st_sf(geometry = st_sfc(st_point(st_coordinates(lines)[1,1:2])),crs = 4326)
+      points <- rbind(start, points) #bring all points together
       xy <- as.data.frame(st_coordinates(points))
-      t<- elevation(longitude = xy$X,latitude = xy$Y, key = apikey)
-      print(a- Sys.time())
+      t<- elevation(longitude = xy$X,latitude = xy$Y, key = apikey) # get elevation
       #
       incProgress(0.2)
       #
@@ -129,19 +124,18 @@ server <- function(input, output, session) {
         } else {
           j <- i +1
           tmp <- distance(point_a = xy[i,] ,point_b = xy[j,], unit = "m") #faster then with st_length
-          #tmp <- st_length(st_linestring(st_coordinates(points[i:j,])))
           points$distance[j] <- tmp
         }
       }
       #
       incProgress(0.2)
       #
-      print(a- Sys.time())
       points$elev <- t$elevation
       points$distance <- cumsum(points$distance)
       points
     })
   } # coords to lines  + adding elv.
+  ##----------------------------------------------------------------------------
   weather <- function(in_data){
     if(class(in_data) == "numeric"){
       mid <- in_data
@@ -161,31 +155,42 @@ server <- function(input, output, session) {
           break
         }
         if(input$route_opt == "cycle"){
-          #print("using cycle")
           tmp[[i]] <- route_cyclestreet(from = data[i,] ,to = data[i+1,] ,
                                         plan = input$plan, pat = cycle_api,
                                         base_url = "https://www.cyclestreets.net")
         }
         else if(input$route_opt == "OSM"){
-          #print("using standard")
           tmp[[i]] <- route_osrm(from = data[i,], to = data[i+1,])
         }
         else if(input$route_opt == "GHopper"){
-          #print("using ghop")
           tmp[[i]] <- route_graphhopper(from = data[i,], to = data[i+1,], pat = graph_hopper,
                                         vehicle = input$graph, silent = T ,base_url = "https://graphhopper.com" )
         }
+        else if(input$route_opt == "ORS"){
+          print(data)
+          coord1 = data[i,]
+          coord2 = data[i+1,]
+          list_coords = list(c(coord1[,1],coord1[,2]), c(coord2[,1],coord2[,2]))
+
+          tmp[[i]] <- st_sf(geojsonsf::geojson_sf(ors_directions(list_coords,
+                                     profile = input$ors_plan, elevation = F,
+                                     format="geojson", parse_output = F))$geometry)
+        }
       }
+
       incProgress(0.1)
+
       route <- do.call(rbind,tmp)
+      if(class(route)[1] != "sf"){
+        route <- sf::st_as_sf(route)
+      }
       return(route)
     })
   } # this is still returning an sp obj.
+  ##----------------------------------------------------------------------------
   search_plc <- function(instring){
     xy <- geo_code(instring)
-    # if(length(xy) == 0){
-    #   xy <- geo_code()
-    # }
+
   }
   height_diff <- function(elev_points, col){
     st_geometry(elev_points) <- NULL
@@ -198,19 +203,20 @@ server <- function(input, output, session) {
       tmp <- tmp + abs(elev_points[i,col] - elev_points[i+1,col])
     }
   }
+  ##----------------------------------------------------------------------------
   performance_km <- function(elev_points, col){
     tmp <- list()
     down <- 0
     up <- 0
     updist <- 0
     data_p <- elev_points
-
     st_geometry(data_p) <- NULL
-
     if(routed$df){
-
+      if(class(tmp_route$df)[1] == "sf"){
+        dist <- as.numeric(st_length(tmp_route$df)/1000)
+      } else {
       dist <- as.numeric(SpatialLinesLengths(tmp_route$df))
-
+      }
     } else {
       dist <- data_p[nrow(data_p),1]
     }
@@ -230,9 +236,9 @@ server <- function(input, output, session) {
         }
       }
     }
-    pKm
-
+    return(pKm)
   }
+  ##----------------------------------------------------------------------------
   traveltime <- function(pkm, speed){
     t <- (pkm/speed)*60
     if(t > 60){
@@ -244,8 +250,40 @@ server <- function(input, output, session) {
       time <- data.frame(h = 0, min = min)
     }
   }
+  ##----------------------------------------------------------------------------
+  iso2sf = function(obj){ #only for iso polygons in
+    interval = c()
+    k = 1
+    poly_list = list()
+    for (i in obj$features){
+      tmp = i
+      tmp2 = data.frame(lon = 0, lat = 0)
+      n = 1
+      for(j in tmp$geometry$coordinates[[1]]){
+        tmp2 = rbind(tmp2, c(lon = j[1], lat = j[2]))
+        n = n + 1
+      }
+      interval[k] = tmp$properties$value
+      poly_list[[k]] = sf::st_sfc(st_polygon(list(as.matrix(tmp2)[-1,])),
+                                  crs = 4326)
+      k = k + 1
+    }
 
+    polys = do.call(c,rev(poly_list))
+    p = sf::st_sf(interval = rev(interval) ,geometry =  polys)
+    return(p)
+  }
 
+  iso_create <- function(x,y,range,profile){
+    range <- range*60
+    t <- c(x,y)
+    iso <- openrouteservice::ors_isochrones(t,range = range, profile = profile,
+                                            interval = range/4, range_type = "time")
+    sf <- iso2sf(iso)
+    return(sf)
+  }
+
+  ##----------------------------------------------------------------------------
 
   #outputs
   observeEvent(help, {
@@ -286,7 +324,19 @@ server <- function(input, output, session) {
       addCircles(lng = if(input$detail){search$df[1]}else{0},
                  lat = if(input$detail){search$df[2]}else{0})
   })
-
+  # leafmap 2 for in reach
+  output$leafmap_reach <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      addDrawToolbar(editOptions = editToolbarOptions(remove = F),singleFeature = T,
+                     circleMarkerOptions = T, circleOptions = F,polygonOptions = F,
+                     rectangleOptions = F,markerOptions = F,polylineOptions = F) %>% addProviderTiles(providers$CartoDB.DarkMatter) %>%    #HikeBike.HikeBike
+      mapview::addMouseCoordinates()  %>%
+      setView(lng =  search$df[1] , lat = search$df[2],
+              zoom =if(input$detail){19} else{12}) %>%
+      addCircles(lng = if(input$detail){search$df[1]}else{0},
+                 lat = if(input$detail){search$df[2]}else{0})
+  })
   # map the routed trip --> map tmp_route
   observe({
     if(!is.null(tmp_route$df)){
@@ -303,6 +353,24 @@ server <- function(input, output, session) {
         setView(lng = view[,1],lat = view[,2] , zoom = 12)
     } else {
       leafletProxy("leafmap", data = c()) %>%
+        clearShapes()
+    }
+  })
+  # reach poly
+  observe({
+    if(!is.null(reach$df)){
+      in_reach <- TRUE
+
+
+      leafletProxy("leafmap_reach", data = reach$df) %>%
+        clearShapes() %>% addPolygons( highlightOptions = highlightOptions(bringToFront = T,
+                                                                           stroke =T,
+                                                                           weight = 5,
+                                                                           color =  "red"),
+                                       color = "black", weight = 1, fillColor = viridisLite::plasma(4,begin = 0.4,end = 1))
+    } else {
+      in_reach <- FALSE
+      leafletProxy("leafmap_reach", data = c()) %>%
         clearShapes()
     }
   })
@@ -338,6 +406,30 @@ server <- function(input, output, session) {
     tmp_route$df <- NULL
     gc()
   })
+  #observer leafmap 2
+  observeEvent(input$leafmap_reach_draw_new_feature,{
+    print("New Feature")
+
+    #x <- input$leafmap_reach_draw_all_features$features[[1]]$geometry$coordinates[[2]]
+    #y <- input$leafmap_reach_draw_all_features$features[[1]]$geometry$coordinates[[1]]
+
+    reach_tf$df <- TRUE
+
+
+  })
+  observe(if(reach_tf$df){
+
+    range <- as.numeric(input$reach_time)
+    profile <- input$reach_plan
+    x <- input$leafmap_reach_draw_all_features$features[[1]]$geometry$coordinates[[2]]
+    y <- input$leafmap_reach_draw_all_features$features[[1]]$geometry$coordinates[[1]]
+    print(range)
+    print(profile)
+    print(x)
+    print(y)
+    reach$df <- iso_create(y,x,range,profile)
+
+    })
 
   # updated for airline trip
   observeEvent(input$leafmap_draw_all_features,{
@@ -397,21 +489,28 @@ server <- function(input, output, session) {
         from <- geo_code(input$from)
         to <- geo_code(input$to)
         all <- rbind(from, wayp)
-        all <- rbind(all, to)
+        all <- data.frame(rbind(all, to))
         #print(all)
         tmp_route$df <- routing(all)
       } else {
-        print("a to b")
+
         from <- geo_code(input$from)
         to <- geo_code(input$to)
-        tmp_route$df <- routing(rbind(from,to))
+        from <- data.frame(x = from[1], y = from[2])
+        to <- data.frame(x = to[1], y = to[2])
+        data <- rbind(from,to)
+
+        tmp_route$df <- routing(data)
       }
 
     } else {
       print("draw route")
       tmp_route$df <- routing(values$df)
+      print(tmp_route$df)
     }
     elevPoints_route$df <- spatial(tmp_route$df)
+    print("elev p")
+    print(elevPoints_route$df)
   })
   # end of routing event
 
@@ -423,6 +522,7 @@ server <- function(input, output, session) {
       height$df <- format(height_diff(elevPoints_route$df, col = "elev"),digits = 5)
       print("pkm")
       pKm$df <- performance_km(elevPoints_route$df,col="elev")
+      print("pkm done")
       pKm$df$total_height <- pKm$df[1,2] + pKm$df[1,3]
       updateProgressBar(session = session, id = "pKm", value = round(pKm$df[1,1],digits = 2),total = pKm$df[1,1])
       updateProgressBar(session = session, id = "flat", value = round(pKm$df[1,4],digits = 2),total = pKm$df[1,1])
