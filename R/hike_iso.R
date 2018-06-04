9c0d4856b4d72f34dd2d4a69e29520227e3a6839
EricKrg <eric.krueger@uni-jena.de>
2018-06-05 00:30:23 +0200

    all functions implemented, adjusted in server.R

---
+++ R/hike_iso.R
@@ -0,0 +1,49 @@
+
+#'
+#' trannsformation function --> geojson from ors to sf
+#'
+#' @param  geojson from ors pckg to sf (only isochrones)
+#' @return sf polygon
+#' @export
+
+
+hike_iso2sf = function(obj){ #only for iso polygons in
+  interval = c()
+  k = 1
+  poly_list = list()
+  for (i in obj$features){
+    tmp = i
+    tmp2 = data.frame(lon = 0, lat = 0)
+    n = 1
+    for(j in tmp$geometry$coordinates[[1]]){
+      tmp2 = rbind(tmp2, c(lon = j[1], lat = j[2]))
+      n = n + 1
+    }
+    interval[k] = tmp$properties$value
+    poly_list[[k]] = sf::st_sfc(st_polygon(list(as.matrix(tmp2)[-1,])),
+                                crs = 4326)
+    k = k + 1
+  }
+
+  polys = do.call(c,rev(poly_list))
+  p = sf::st_sf(interval = rev(interval) ,geometry =  polys)
+  return(p)
+}
+
+
+#'
+#' creates a iso chrone polygon for reachabilty display
+#'
+#' @param  num coords, range, and profile(moving style)
+#' @return sf polygon
+#' @export
+
+
+iso_create <- function(x,y,range,profile){
+  range <- range*60
+  t <- c(x,y)
+  iso <- openrouteservice::ors_isochrones(t,range = range, profile = profile,
+                                          interval = range/4, range_type = "time")
+  sf <- hikeR::hike_iso2sf(iso)
+  return(sf)
+}
--- R/hike_routing.R
+++ R/hike_routing.R
@@ -2,8 +2,8 @@
 #' this function combines multiple routing providers and their different routing styles
 #'
 #' routing
-#' @return return simple weather forecast by coords
-#' @param  data frame of coords, shiny progress, profile and api
+#' @return returns sf linestring
+#' @param  dataframe of coords, shiny progress (bool), profile and api
 #' @export
 
 hike_routing <- function(data, shiny_progress,provider,profile,api){
@@ -20,22 +20,19 @@ hike_routing <- function(data, shiny_progress,provider,profile,api){
           break
         }
         if(provider == "cycle"){
-          print("cycle")
           tmp[[i]] <- route_cyclestreet(from = data[i,] ,to = data[i+1,] ,
                                         plan = profile, pat = api,
                                         base_url = "https://www.cyclestreets.net")
         }
         else if(provider == "OSM"){
-          print("route OSM")
-          print(data)
-          tmp[[i]] <- route_osrm(from = data[i,], to = data[i+1,])
+          data <- as.matrix(data)
+          tmp[[i]] <- route_osrm(from = as.numeric(data[i,]), to = as.numeric(data[i+1,]))
         }
         else if(provider == "GHopper"){
           tmp[[i]] <- route_graphhopper(from = data[i,], to = data[i+1,], pat = api,
                                         vehicle = profile, silent = T ,base_url = "https://graphhopper.com" )
         }
         else if(provider == "ORS"){
-          print("ORS")
           ors_api_key(api)
           coord1 = data[i,]
           coord2 = data[i+1,]
@@ -67,7 +64,8 @@ hike_routing <- function(data, shiny_progress,provider,profile,api){
                                       base_url = "https://www.cyclestreets.net")
       }
       else if(provider == "OSM"){
-        tmp[[i]] <- route_osrm(from = data[i,], to = data[i+1,])
+        data <- as.matrix(data)
+        tmp[[i]] <- route_osrm(from = as.numeric(data[i,]), to = as.numeric(data[i+1,]))
       }
       else if(provider == "GHopper"){
         tmp[[i]] <- route_graphhopper(from = data[i,], to = data[i+1,], pat = api,
@@ -92,5 +90,9 @@ hike_routing <- function(data, shiny_progress,provider,profile,api){
   }
 }
 
-#' data <- data.frame(geo_code("Jena"),geo_code("Weimar"))
-#' hikeR::hike_routing(data,shiny_progress = F,provider = "cycle",profile = "fastest",api = Sys.getenv("cycle_api"))
+
+#' a <- stplanr::geo_code("jena")
+#' b <- stplanr::geo_code("weimar")
+#' data <- data.frame(x = c(a[1],b[1]), y = c(a[2], b[2]))
+#' a_to_b <- hikeR::hike_routing(data,shiny_progress = F,provider = "cycle",profile = "fastest",api = Sys.getenv("cycle_api"))
+#' mapview::mapview(a_to_b)
