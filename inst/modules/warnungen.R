7aa511f95dfca2e3e43f626baf47e476add824e3
Eric_Krg <eric.krueger@uni-jena.de>
2018-06-15 10:21:16 +0200

    added trycatch if there are no weather warnings

--- inst/modules/warnungen.R
+++ inst/modules/warnungen.R
@@ -1,14 +1,23 @@
 library(sf)
 library(dplyr)
+library(stringr)
 
 
-warnungen = function(search) {
-  ogrListLayers(dsn)
+warnungen = function() {
+  #ogrListLayers("https://maps.dwd.de/geoserver/ows?service=wfs&version=2.0.0&request=GetCapabilities")
   # ogrinfo(dsn, so=TRUE)
 
-  warnungen = st_read("WFS:https://maps.dwd.de/geoserver/ows?service=wfs&version=2.0.0&request=GetCapabilities",
-                 layer = "dwd:Warnungen_Gemeinden") %>%
+  warnungen = tryCatch({
+    warnungen = st_read("WFS:https://maps.dwd.de/geoserver/ows?service=wfs&version=2.0.0&request=GetCapabilities",
+    layer = "dwd:Warnungen_Gemeinden",
+    quiet = T) %>%
     st_cast("GEOMETRYCOLLECTION") %>% st_collection_extract("POLYGON") %>%
-    select(EVENT)
+    dplyr::select(EVENT, DESCRIPTION, SENT, PARAMATERVALUE, WEB) %>%
+    dplyr::mutate(EVENT = as.factor(stringr::str_to_title(as.character(EVENT))))
+    },
+    error=function(cond) {
+      message("No weather warnings")
+      no = TRUE
+    })
   return(warnungen)
 }
